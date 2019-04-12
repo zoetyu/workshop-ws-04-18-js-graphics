16973e8b09fbcd94797b9e301441647e5a3e883c
Will Chisholm <william.f.chisholm.iii.20@dartmouth.edu>
2019-04-11 23:20:31 -0400

    step1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import './style.scss';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let num = 0;
 setInterval(() => {
